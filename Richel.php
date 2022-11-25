24b9dd046fd9df3ca015cf362129d836bf81033b
Richel <21103788@usc.edu.ph>
2022-11-25 19:57:57 +0800

    The final output for login UI for this week

--- Richel.php
+++ Richel.php
@@ -12,11 +12,11 @@ if (isset($_GET['message'])) {
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -80,7 +80,7 @@ if (isset($_GET['message'])) {
         display: none;
         justify-content: center;
         align-items: center;
-        /* opacity: 80%; */
+
     }
 
     .popup-content {
